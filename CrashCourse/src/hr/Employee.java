db6a2be31943d34c01f8ed7c007af9309d8792f6
Akonicki26 <anne.konicki@mymail.champlain.edu>
2026-01-21 09:28:49 -0500

    Add main method in test file

--- CrashCourse/src/hr/Employee.java
+++ CrashCourse/src/hr/Employee.java
@@ -1,3 +1,5 @@
+package hr;
+
 import java.time.LocalDate;
 
 public class Employee {
---
+++ CrashCourse/src/test/TestDrive.java
@@ -0,0 +1,7 @@
+package test;
+
+public class TestDrive {
+    public static void main(String[] args) {
+
+    }
+}
